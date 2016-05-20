ab38dff94b50912992b91d897d60bb15b01aa88a
Ken <ken.deeley@mathworks.co.uk>
2016-05-20 15:01:29 +0100

    Started adding phase-plane arrows.

--- plotPhasePortrait.m
+++ plotPhasePortrait.m
@@ -34,20 +34,75 @@ dC = ((RCK_df(K, params) - ...
 dC(~isfinite(dC)) = 0;
 
 %% Visualize the phase portrait.
-figure
+figure('Units', 'Normalized', 'Position', 0.25*[1, 1, 2, 2])
 h = streamslice(K, C, dK, dC, 2, 'noarrows', 'cubic');
-set(h, 'LineStyle', ':')
+set(h, 'LineStyle', ':', 'LineWidth', 1.5)
 % Plot the steady-state vertical line and smooth curve.
 hold on
 plot(k_fine, c_star, 'm', 'LineWidth', 2)
-xlabel('k (capital per capita)')
-ylabel('c (consumption per capita)')
+xlabel('{\it{k}} (capital per capita)')
+ylabel('{\it{c}} (consumption per capita)')
 title('Ramsey/Cass-Koopmans Capital-Consumption Phase Plane')
 grid
 plot([params.k_steady, params.k_steady], ylim(), 'k', 'LineWidth', 2)
+% Increase the font size.
+ax = gca;
+ax.FontSize = 14;
 
-%% TODO: add the direction arrows (four regions) and labels, similar to
-% the Wikipedia article.
+%% Add the direction arrows (four regions) and labels.
 
+% Top-right quadrant.
+% Coordinates.
+x = [80, 80];
+y = [2.5, 2.2];
+[xFig, yFig] = ds2nfu(x, y);
+% Annotation.
+annotation('arrow', xFig, yFig, 'LineWidth', 1.5)
+% Coordinates.
+x = [80, 73];
+y = [2.5, 2.5];
+[xFig, yFig] = ds2nfu(x, y);
+% Annotation.
+annotation('arrow', xFig, yFig, 'LineWidth', 1.5)
 
+% Top-left quadrant.
+% Coordinates.
+x = [20, 20];
+y = [2.6, 2.9];
+[xFig, yFig] = ds2nfu(x, y);
+% Annotation.
+annotation('arrow', xFig, yFig, 'LineWidth', 1.5)
+% Coordinates.
+x = [20, 13];
+y = [2.6, 2.6];
+[xFig, yFig] = ds2nfu(x, y);
+% Annotation.
+annotation('arrow', xFig, yFig, 'LineWidth', 1.5)
 
+% Bottom-right quadrant.
+% Coordinates.
+x = [80, 80];
+y = [2.5, 2.2];
+[xFig, yFig] = ds2nfu(x, y);
+% Annotation.
+annotation('arrow', xFig, yFig, 'LineWidth', 1.5)
+% Coordinates.
+x = [80, 73];
+y = [2.5, 2.5];
+[xFig, yFig] = ds2nfu(x, y);
+% Annotation.
+annotation('arrow', xFig, yFig, 'LineWidth', 1.5)
+
+% Bottom-left quadrant.
+% Coordinates.
+x = [80, 80];
+y = [2.5, 2.2];
+[xFig, yFig] = ds2nfu(x, y);
+% Annotation.
+annotation('arrow', xFig, yFig, 'LineWidth', 1.5)
+% Coordinates.
+x = [80, 73];
+y = [2.5, 2.5];
+[xFig, yFig] = ds2nfu(x, y);
+% Annotation.
+annotation('arrow', xFig, yFig, 'LineWidth', 1.5)
